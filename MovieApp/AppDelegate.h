17b3c1c5d6208652e8694f2823fe482f2590061b
afnan8 <afnan.hassan1993@gmail.com>
2019-03-02 15:00:01 +0200

    Initial Commit

---
+++ MovieApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MovieApp
+//
+//  Created by afnan on 3/2/19.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MovieApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MovieApp
+//
+//  Created by afnan on 3/2/19.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
