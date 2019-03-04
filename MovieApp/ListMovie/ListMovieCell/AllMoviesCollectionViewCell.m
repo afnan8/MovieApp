ebcbf954836e8e45b83e3426a9a57b65b664c33c
afnan8 <afnan.hassan1993@gmail.com>
2019-03-04 09:18:07 +0200

    list movie

---
+++ MovieApp/ListMovie/ListMovieCell/AllMoviesCollectionViewCell.m
@@ -0,0 +1,96 @@
+//
+//  AllMoviesCollectionViewCell.m
+//  MoviesApp
+//
+//  Created by afnan on 2/28/19.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+#import "AllMoviesCollectionViewCell.h"
+
+@implementation AllMoviesCollectionViewCell
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    self = [super initWithFrame:frame];
+    if (self) {
+        _didSetupConstraints= NO;
+        [self initSubViews];
+        [self addSubviews];
+        [self setNeedsUpdateConstraints];
+    }
+    return self;
+}
+
+-(void) initSubViews {
+    //movie poster
+    _movieImageView = [[UIImageView alloc] init];
+    [_movieImageView setTranslatesAutoresizingMaskIntoConstraints: NO];
+    _movieImageView.clipsToBounds = YES;
+    _movieImageView.layer.masksToBounds = YES;
+    _movieImageView.backgroundColor = UIColor.whiteColor;
+    [_movieImageView setContentMode: UIViewContentModeScaleToFill];
+ 
+    //movie title
+    _titleLabel = [[UILabel alloc] init];
+    [_titleLabel setTranslatesAutoresizingMaskIntoConstraints: NO];
+    _titleLabel.font = [UIFont boldSystemFontOfSize:16];
+    _titleLabel.textColor = UIColor.whiteColor;
+    _titleLabel.numberOfLines = 0;
+    _titleLabel.adjustsFontSizeToFitWidth = YES;
+    
+    //movie overview
+    _overviewLabel = [[UILabel alloc] init];
+    [_overviewLabel setTranslatesAutoresizingMaskIntoConstraints: NO];
+    _overviewLabel.font = [UIFont systemFontOfSize:15];
+    _overviewLabel.textColor = UIColor.appTextColor;
+    _overviewLabel.numberOfLines = 3;
+    
+    //date label
+    _dateLabel = [[UILabel alloc] init];
+    [_dateLabel setTranslatesAutoresizingMaskIntoConstraints: NO];
+    _dateLabel.font = [UIFont boldSystemFontOfSize:12];
+    _dateLabel.textColor = UIColor.appTextColor;
+    _dateLabel.numberOfLines = 0;
+}
+
+-(void) addSubviews {
+    [self addSubview: _movieImageView];
+    [self addSubview:_titleLabel];
+    [self addSubview: _overviewLabel];
+    [self addSubview: _dateLabel];
+}
+
+-(void)prepareForReuse {
+    [super prepareForReuse];
+    _titleLabel.text = @"";
+    _overviewLabel.text = @"";
+    _dateLabel.text = @"";
+    _movieImageView.image= [[UIImage alloc] init];
+}
+- (void)updateConstraints {
+    if (!_didSetupConstraints) {
+        //movie poster layout
+        [_movieImageView.leadingAnchor constraintEqualToAnchor: self.leadingAnchor constant: 15].active = YES;
+        [_movieImageView.widthAnchor constraintEqualToAnchor: self.widthAnchor multiplier:0.333].active = YES;
+        [_movieImageView.topAnchor constraintEqualToAnchor: self.topAnchor constant: 10].active = YES;
+        [_movieImageView.bottomAnchor constraintEqualToAnchor: self.bottomAnchor constant: -10].active = YES;
+        //movie title layout
+        [_titleLabel.leadingAnchor constraintEqualToAnchor: _movieImageView.trailingAnchor constant: 15].active = YES;
+        [_titleLabel.trailingAnchor constraintEqualToAnchor: self.trailingAnchor constant: -15].active = YES;
+        [_titleLabel.topAnchor constraintEqualToAnchor: _movieImageView.topAnchor].active = YES;
+        [_titleLabel.heightAnchor constraintEqualToAnchor: self.heightAnchor multiplier: 0.25].active = YES;
+        //movie date
+        [_dateLabel.leadingAnchor constraintEqualToAnchor: _movieImageView.trailingAnchor constant: 15].active = YES;
+        [_dateLabel.trailingAnchor constraintEqualToAnchor: self.trailingAnchor constant: -15].active = YES;
+        [_dateLabel.topAnchor constraintEqualToAnchor: _titleLabel.bottomAnchor constant: 3].active = YES;
+        [_dateLabel.heightAnchor constraintEqualToConstant: 20].active = YES;
+        //movie overview
+        [_overviewLabel.leadingAnchor constraintEqualToAnchor: _movieImageView.trailingAnchor constant: 15].active = YES;
+        [_overviewLabel.trailingAnchor constraintEqualToAnchor: self.trailingAnchor constant: -15].active = YES;
+        [_overviewLabel.bottomAnchor constraintEqualToAnchor: _movieImageView.bottomAnchor].active = YES;
+        [_overviewLabel.topAnchor constraintEqualToAnchor: _dateLabel.bottomAnchor].active = YES;
+    }
+    _didSetupConstraints = YES;
+    [super updateConstraints];
+}
+@end
---
+++ MovieApp/ListMovie/ListMovieView.m
@@ -0,0 +1,81 @@
+//
+//  ListMovieView.m
+//  MovieApp
+//
+//  Created by afnan on 3/3/19.
+//  Copyright © 2019 macbook pro. All rights reserved.
+//
+
+#import "ListMovieView.h"
+
+
+@implementation ListMovieView
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _cellIdentifier = @"CellIdentifier";
+        _footerIdentifier = @"footerIdentifier";
+    }
+    return self;
+}
+-(void) initCollectionView {
+    UICollectionViewFlowLayout *layout=[[UICollectionViewFlowLayout alloc] init];
+    
+    _collectionView = [[UICollectionView alloc] initWithFrame: CGRectNull collectionViewLayout:layout];
+    [_collectionView setTranslatesAutoresizingMaskIntoConstraints:NO];
+    [self addSubview: _collectionView];
+    [_collectionView.leadingAnchor constraintEqualToAnchor: self.leadingAnchor].active = YES;
+    [_collectionView.trailingAnchor constraintEqualToAnchor: self.trailingAnchor].active = YES;
+    [_collectionView.topAnchor constraintEqualToAnchor: self.topAnchor].active = YES;
+    [_collectionView.bottomAnchor constraintEqualToAnchor: self.bottomAnchor].active = YES;
+    _collectionView.delegate = self;
+    _collectionView.dataSource = self;
+    _collectionView.backgroundColor = [UIColor appColor];
+    [_collectionView registerClass: [AllMoviesCollectionViewCell class] forCellWithReuseIdentifier: _cellIdentifier];
+    [_collectionView registerClass:[UICollectionReusableView class] forSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier: _footerIdentifier];
+}
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+- (NSInteger)collectionView:(nonnull UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return 0;
+}
+
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
+    return CGSizeMake(collectionView.frame.size.width - 40, collectionView.frame.size.height * 0.25 - 10);
+}
+
+- (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section {
+    return  UIEdgeInsetsMake(5, 20, 5, 20);
+}
+
+- (CGFloat)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout minimumLineSpacingForSectionAtIndex:(NSInteger)section {
+    return 10;
+}
+
+- (nonnull __kindof UICollectionViewCell *)collectionView:(nonnull UICollectionView *)collectionView cellForItemAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    AllMoviesCollectionViewCell *cell= [collectionView dequeueReusableCellWithReuseIdentifier: _cellIdentifier forIndexPath:indexPath];
+    cell.backgroundColor = [UIColor lightColor];
+    return cell;
+}
+
+- (UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionReusableView *footerView = [collectionView dequeueReusableSupplementaryViewOfKind:UICollectionElementKindSectionFooter withReuseIdentifier: _footerIdentifier forIndexPath:indexPath];
+    _activityIndicatorView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    _activityIndicatorView.tag = 10;
+    _activityIndicatorView.center = CGPointMake(footerView.bounds.size.width / 2.0f, footerView.bounds.size.height / 2.0f);
+    _activityIndicatorView.autoresizingMask = UIViewAutoresizingFlexibleLeftMargin | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleRightMargin | UIViewAutoresizingFlexibleBottomMargin;
+    _activityIndicatorView.hidesWhenStopped = YES;
+    [footerView addSubview: _activityIndicatorView];
+    
+    return footerView;
+}
+- (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout referenceSizeForFooterInSection:(NSInteger)section {
+    return CGSizeMake(collectionView.bounds.size.width, 40);
+}
+
+@end
